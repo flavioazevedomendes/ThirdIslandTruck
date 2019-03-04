5ca8255b41b79ce6597f158313aaeda576237f2a
DarioSousa <justtoofnhotmail.com>
2019-03-04 15:57:57 -0100

    ant try

--- src/org/academiadecodigo/ThirdIslandTruck/Obstacles/Cow.java
+++ src/org/academiadecodigo/ThirdIslandTruck/Obstacles/Cow.java
@@ -6,7 +6,7 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Cow extends GameObstacle {
 
     public Cow() {
-        super(new Picture( 948, Util.getRandom(313, 650), "resources/Cow2.png"));
+        super(new Picture( 948, Util.getRandom(313, 650), "resources/cow2.png"));
 
     }
 }
